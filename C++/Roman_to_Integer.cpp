b487d07376c38e15fd7d5091fe6ad9d2c0901f96
Tanzeela Fatima <149133736+Fatima-progmmer@users.noreply.github.com>
2025-08-07 23:39:02 +0500

    close(#508).cpp
    🔢 Added C++ solution for LeetCode Problem 13: Roman to Integer
    
    - Used unordered_map for Roman numeral values
    - Traversed string from right to left
    - Time Complexity: O(n), Space Complexity: O(1)
    - Included comments and a sample test case
    
    Closes #508

--- C++/Roman_to_Integer.cpp
+++ C++/Roman_to_Integer.cpp
@@ -1,60 +1,32 @@
-#include <string>
-
-/*** 13. Roman to Intege (Easy)***/
-
-/*
-Symbol       Value
-I             1
-V             5
-X             10
-L             50
-C             100
-D             500
-M             1000
-*/
+// LeetCode Problem 13: Roman to Integer
+// Author: Tanzeela Fatima (@Fatima-progmmer)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
 class Solution {
 public:
     int romanToInt(string s) {
-        int current = 0, last =0;
-        int sum=0;
-        for(int i=0;i<s.length();i++){
-            switch(s[i]){
-                case 'I':
-                    current = 1;
-                    break;
-                case 'V':
-                    current = 5;
-                    break;
-                case 'X':
-                    current = 10;
-                    break;
-                case 'L':
-                    current = 50;
-                    break;
-                case 'C':
-                    current = 100;
-                    break;
-                case 'D':
-                    current = 500;
-                    break;
-                case 'M':
-                    current = 1000;
-                    break;
-                default:
-                  break;
-            }
-            sum+=current;
-            /*
-            I can be placed before V (5) and X (10) to make 4 and 9. 
-            X can be placed before L (50) and C (100) to make 40 and 90. 
-            C can be placed before D (500) and M (1000) to make 400 and 900
-            */
-            if(i!=0&&current>last)
-                sum-=2*last;
-            last = current;
-            
+        unordered_map<char, int> roman = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+
+        int result = 0;
+        int prev = 0;
+
+        // Traverse from right to left
+        for (int i = s.length() - 1; i >= 0; --i) {
+            int current = roman[s[i]];
+
+            if (current < prev)
+                result -= current;
+            else
+                result += current;
+
+            prev = current;
         }
-        return sum;
+
+        return result;
     }
 };
+
